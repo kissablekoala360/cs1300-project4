1f04260f9ffd3cf771fee8157620a7dac786aa05
john <jfinberg@haverford.edu>
2022-10-18 10:05:19 -0400

    made revisions

--- assets/scripts/scroll-animations.js
+++ assets/scripts/scroll-animations.js
@@ -21,3 +21,25 @@ $(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+$('#proto-phone').click(function(){
+  $('#img01').attr("src", "./assets/images/wireframes/lowfi/Mobile.png");
+  $("#myModal").show();
+})
+
+$('#proto-tablet').click(function(){
+  $('#img01').attr("src", "./assets/images/wireframes/lowfi/Tablet.png");
+  $("#myModal").show();
+})
+
+$('#proto-desktop').click(function(){
+  $('#img01').attr("src", "./assets/images/wireframes/lowfi/Desktop.png");
+  $("#myModal").show();
+})
+
+// When the user clicks on <span> (x), close the modal
+$('#close-modal').click(function(){
+    $("#myModal").hide();
+});
